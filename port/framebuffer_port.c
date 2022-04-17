75eb3fbcc15a3305e0f066f446804e49a2de4b41
Aysi-E <773917760@qq.com>
2022-04-17 23:44:14 +0800

    become run ico generate way

--- port/framebuffer_port.c
+++ port/framebuffer_port.c
@@ -195,40 +195,7 @@ void MTF_fb_scale(framebuffer_dev_type *fb, uint8_t state, void *data) //是否�
     // fb->scale_flag = 1; //开启缩放
 }
 
-static uint16_t ico_pixels[16 * 16] = {// ...or with raw pixel data:
-                            0x0fff, 0x0fff, 0x0fff, 0x0fff, 0x0fff, 0x0fff, 0x0fff, 0x0fff,
-                            0x0fff, 0x0fff, 0x0fff, 0x0fff, 0x0fff, 0x0fff, 0x0fff, 0x0fff,
-                            0x0fff, 0x0fff, 0x0fff, 0x0fff, 0x0fff, 0x0fff, 0x0fff, 0x0fff,
-                            0x0fff, 0x0fff, 0x0fff, 0x0fff, 0x0fff, 0x0fff, 0x0fff, 0x0fff,
-                            0x0fff, 0x0fff, 0x0fff, 0x0fff, 0x0fff, 0x0fff, 0x0fff, 0x0fff,
-                            0x0fff, 0x0fff, 0x0fff, 0x0fff, 0x0fff, 0x0fff, 0x0fff, 0x0fff,
-                            0x0fff, 0x0aab, 0x0789, 0x0bcc, 0x0eee, 0x09aa, 0x099a, 0x0ddd,
-                            0x0fff, 0x0eee, 0x0899, 0x0fff, 0x0fff, 0x1fff, 0x0dde, 0x0dee,
-                            0x0fff, 0xabbc, 0xf779, 0x8cdd, 0x3fff, 0x9bbc, 0xaaab, 0x6fff,
-                            0x0fff, 0x3fff, 0xbaab, 0x0fff, 0x0fff, 0x6689, 0x6fff, 0x0dee,
-                            0xe678, 0xf134, 0x8abb, 0xf235, 0xf678, 0xf013, 0xf568, 0xf001,
-                            0xd889, 0x7abc, 0xf001, 0x0fff, 0x0fff, 0x0bcc, 0x9124, 0x5fff,
-                            0xf124, 0xf356, 0x3eee, 0x0fff, 0x7bbc, 0xf124, 0x0789, 0x2fff,
-                            0xf002, 0xd789, 0xf024, 0x0fff, 0x0fff, 0x0002, 0x0134, 0xd79a,
-                            0x1fff, 0xf023, 0xf000, 0xf124, 0xc99a, 0xf024, 0x0567, 0x0fff,
-                            0xf002, 0xe678, 0xf013, 0x0fff, 0x0ddd, 0x0fff, 0x0fff, 0xb689,
-                            0x8abb, 0x0fff, 0x0fff, 0xf001, 0xf235, 0xf013, 0x0fff, 0xd789,
-                            0xf002, 0x9899, 0xf001, 0x0fff, 0x0fff, 0x0fff, 0x0fff, 0xe789,
-                            0xf023, 0xf000, 0xf001, 0xe456, 0x8bcc, 0xf013, 0xf002, 0xf012,
-                            0x1767, 0x5aaa, 0xf013, 0xf001, 0xf000, 0x0fff, 0x7fff, 0xf124,
-                            0x0fff, 0x089a, 0x0578, 0x0fff, 0x089a, 0x0013, 0x0245, 0x0eff,
-                            0x0223, 0x0dde, 0x0135, 0x0789, 0x0ddd, 0xbbbc, 0xf346, 0x0467,
-                            0x0fff, 0x4eee, 0x3ddd, 0x0edd, 0x0dee, 0x0fff, 0x0fff, 0x0dee,
-                            0x0def, 0x08ab, 0x0fff, 0x7fff, 0xfabc, 0xf356, 0x0457, 0x0467,
-                            0x0fff, 0x0bcd, 0x4bde, 0x9bcc, 0x8dee, 0x8eff, 0x8fff, 0x9fff,
-                            0xadee, 0xeccd, 0xf689, 0xc357, 0x2356, 0x0356, 0x0467, 0x0467,
-                            0x0fff, 0x0ccd, 0x0bdd, 0x0cdd, 0x0aaa, 0x2234, 0x4135, 0x4346,
-                            0x5356, 0x2246, 0x0346, 0x0356, 0x0467, 0x0356, 0x0467, 0x0467,
-                            0x0fff, 0x0fff, 0x0fff, 0x0fff, 0x0fff, 0x0fff, 0x0fff, 0x0fff,
-                            0x0fff, 0x0fff, 0x0fff, 0x0fff, 0x0fff, 0x0fff, 0x0fff, 0x0fff,
-                            0x0fff, 0x0fff, 0x0fff, 0x0fff, 0x0fff, 0x0fff, 0x0fff, 0x0fff,
-                            0x0fff, 0x0fff, 0x0fff, 0x0fff, 0x0fff, 0x0fff, 0x0fff, 0x0fff};
-
+#include "UI_engine.h" //use to run ico
 uint8_t MTF_fb_init(framebuffer_dev_type *fb)
 {   
     //SDL相关在SDL_Init统一初始化
@@ -244,12 +211,19 @@ uint8_t MTF_fb_init(framebuffer_dev_type *fb)
         DEBUG_FRAMEBUFFER("SDL: could not create window - exiting:%s\n", SDL_GetError());
         return 2;
     }
-    
-    SDL_Surface *surface;     // Declare an SDL_Surface to be filled in with pixel data from an image file
-    surface = SDL_CreateRGBSurfaceFrom(ico_pixels, 16, 16, 16, 16 * 2, 0x0f00, 0x00f0, 0x000f, 0xf000);
 
-    // The icon is attached to the screen pointer
-    SDL_SetWindowIcon(screen, surface);
+    //use to run ico
+    RectInfo pic;
+    pic.pixelByte = 4;
+    if (png_decode("./project_ico.png", &pic) == 0)
+    {
+        SDL_Surface *surface; // Declare an SDL_Surface to be filled in with pixel data from an image file
+        surface = SDL_CreateRGBSurfaceFrom(pic.pixelDatas, pic.width, pic.height, pic.pixelByte*8, 
+                            pic.width * pic.pixelByte, 0x00ff0000, 0x0000ff00, 0x000000ff, 0xff000000);
+        
+        // The icon is attached to the screen pointer
+        SDL_SetWindowIcon(screen, surface);
+    }
 
     SDL_CreateThread(_SDL_flush, NULL, NULL); //创建线程, 用于定时更新显示
 
@@ -276,12 +250,19 @@ uint8_t MTF_fb_reset(framebuffer_dev_type *fb)
         DEBUG_FRAMEBUFFER("SDL: could not create window - exiting:%s\n", SDL_GetError());
         return 2;
     }
-    
-    SDL_Surface *surface;     // Declare an SDL_Surface to be filled in with pixel data from an image file
-    surface = SDL_CreateRGBSurfaceFrom(ico_pixels, 16, 16, 16, 16 * 2, 0x0f00, 0x00f0, 0x000f, 0xf000);
 
-    // The icon is attached to the screen pointer
-    SDL_SetWindowIcon(screen, surface);
+    //use to run ico
+    RectInfo pic;
+    pic.pixelByte = 4;
+    if (png_decode("./project_ico.png", &pic) == 0)
+    {
+        SDL_Surface *surface; // Declare an SDL_Surface to be filled in with pixel data from an image file
+        surface = SDL_CreateRGBSurfaceFrom(pic.pixelDatas, pic.width, pic.height, pic.pixelByte*8, 
+                            pic.width * pic.pixelByte, 0x00ff0000, 0x0000ff00, 0x000000ff, 0xff000000);
+        
+        // The icon is attached to the screen pointer
+        SDL_SetWindowIcon(screen, surface);
+    }
 
     return 0;
 }
