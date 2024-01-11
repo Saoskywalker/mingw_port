d35ab0f84d503699fda4d6fd42ee69d175baad7f
Saoskywalker <773917760@qq.com>
2024-01-11 11:13:00 +0800

    add copyright declare

--- port/file_operate_hal.c
+++ port/file_operate_hal.c
@@ -1,3 +1,16 @@
+/*
+Copyright (c) 2019-2023 Aysi 773917760@qq.com. All right reserved
+Official site: www.mtf123.club
+
+This software is provided 'as-is', without any express or implied
+warranty. In no event will the authors be held liable for any damages
+arising from the use of this software.
+
+It under the terms of the Apache as published;
+either version 2 of the License,
+or (at your option) any later version.
+*/
+
 #include "file_operate_hal.h" 
 #include "MTF_io.h"
 #include "malloc.h"		  
